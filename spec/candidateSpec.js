62387a2bf77fe90f4fbe9fa77e68e929c835d3f9
Your Name <teakowg@gmail.com>
2017-03-08 12:46:00 +0000

    Testing candidate object can receive user votes

---
+++ spec/candidateSpec.js
@@ -0,0 +1,20 @@
+describe('Candidate', function() {
+
+  var candidate;
+  var user;
+
+  beforeEach(function() {
+    candidate = new Candidate('Dante');
+    user = jasmine.createSpyObj('user', ['castVote']);
+  });
+
+  it('should be initialized with a name for identification', function() {
+    expect(candidate.name).toEqual('Dante');
+  });
+
+  it('should receive votes cast by users', function() {
+    user.castVote(candidate);
+    expect(candidate.votes).toEqual(1);
+  });
+
+});
--- spec/userSpec.js
+++ spec/userSpec.js
@@ -1,15 +1,16 @@
 describe('User', function() {
 
   var user;
+  var candidate;
 
   beforeEach(function() {
     user = new User('Teako');
     candidate = jasmine.createSpyObj('candidate', ['receiveVote']);
-  }) // spyObjects manage dependancies and separation of concerns. beforeEach block keeps the setup of each test easier to read & less cluttered.
+  }); // spyObjects manage dependancies and separation of concerns. beforeEach block keeps the setup of each test easier to read & less cluttered.
 
   it('should be initialized with a username for identification', function() {
     expect(user.name).toEqual('Teako');
-  })
+  });
 
   it('should be able to cast a vote for a candidate', function() {
     user.castVote(candidate);
